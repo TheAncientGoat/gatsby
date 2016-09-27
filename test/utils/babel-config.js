376888654b64a9f45bdd69571e2b7bb32d85d333
Ryan Swart <ryanswrt@gmail.com>
2016-09-27 10:14:38 +0800

    switch to react-hot-loader@3.0.0-beta-5 for HMR - still does not HMR exported stateless components however

--- test/utils/babel-config.js
+++ test/utils/babel-config.js
@@ -8,7 +8,7 @@ function programStub (fixture) {
   return { directory }
 }
 
-test('it returns a default babel config for babel-loader query', t => {
+test('it returns a default babel config for babel-loader query', (t) => {
   const program = programStub('site-without-babelrc')
   const config = babelConfig(program)
 
@@ -17,7 +17,7 @@ test('it returns a default babel config for babel-loader query', t => {
   t.truthy(config.plugins.length)
 })
 
-test('all plugins are absolute paths to avoid babel lookups', t => {
+test('all plugins are absolute paths to avoid babel lookups', (t) => {
   const program = programStub('site-without-babelrc')
   const config = babelConfig(program)
 
@@ -25,36 +25,37 @@ test('all plugins are absolute paths to avoid babel lookups', t => {
   config.plugins.forEach(plugin => t.true(path.resolve(plugin) === plugin))
 })
 
-test('fixture can resolve plugins in gatsby directory (crawling up)', t => {
+test('fixture can resolve plugins in gatsby directory (crawling up)', (t) => {
   const program = programStub('site-with-valid-babelrc')
 
   const config = babelConfig(program)
   t.truthy(config.presets.length)
 })
 
-test('throws error when babelrc is not parseable', t => {
+test('throws error when babelrc is not parseable', (t) => {
   const program = programStub('site-with-invalid-babelrc')
 
   t.throws(() => babelConfig(program))
 })
 
-test('can read babel from packagejson', t => {
+test('can read babel from packagejson', (t) => {
   const program = programStub('site-with-valid-babelpackage')
 
   const config = babelConfig(program)
   t.truthy(config.presets.length)
 })
 
-test('when in development has hmre', t => {
+test('when in development has hmre', (t) => {
   const program = programStub('site-without-babelrc')
   const config = babelConfig(program, 'develop')
-
+  console.log(config.plugins)
   // regex matches: babel followed by any amount of hyphen or word characters
-  const presetNames = config.presets.map(p => p.match(/babel[-|\w]+/)[0])
-  t.true(includes(presetNames, 'babel-preset-react-hmre'))
+  const plugins = config.plugins.map(p => p.match(/react[-|\w]+/))
+  const pluginNames = plugins.map((p) => { if (p) { return p[0] } else { return p } })
+  t.true(includes(pluginNames, 'react-hot-loader'))
 })
 
-test('throws when a plugin is not available', t => {
+test('throws when a plugin is not available', (t) => {
   const program = programStub('site-with-unresolvable-babelrc')
   t.throws(() => babelConfig(program))
 })
